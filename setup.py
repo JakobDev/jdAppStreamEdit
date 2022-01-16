31ec3f9230d7cd546b935fcac888aa88dee77997
JakobDev <jakobdev@gmx.de>
2022-01-16 11:49:18 +0100

    Update to Version 2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 
 setup(name="jdAppdataEdit",
-    version="2.0",
+    version="2.1",
     description="A graphical Program to create and edit Appdata files",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
