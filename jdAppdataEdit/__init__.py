9851159193420cee68c0145e22b202ec2881a239
JakobDev <jakobdev@gmx.de>
2022-07-05 09:49:34 +0000

    Load Qt translations

--- jdAppdataEdit/__init__.py
+++ jdAppdataEdit/__init__.py
@@ -1,6 +1,6 @@
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtCore import QTranslator, QLocale
+from PyQt6.QtCore import QTranslator, QLocale, QLibraryInfo
 from .TranslateWindow import TranslateWindow
+from PyQt6.QtWidgets import QApplication
 from .MainWindow import MainWindow
 from .Enviroment import Enviroment
 import argparse
@@ -16,15 +16,23 @@ def main():
     app.setApplicationName("jdAppdataEdit")
     app.setWindowIcon(env.icon)
 
-    translator = QTranslator()
+    app_translator = QTranslator()
+    qt_translator = QTranslator()
+    app_trans_dir = os.path.join(env.program_dir, "i18n")
+    qt_trans_dir = QLibraryInfo.path(QLibraryInfo.LibraryPath.TranslationsPath)
     language = env.settings.get("language")
     if language == "default":
         system_language = QLocale.system().name()
-        translator.load(os.path.join(env.program_dir, "i18n", "jdAppdataEdit_" + system_language.split("_")[0] + ".qm"))
-        translator.load(os.path.join(env.program_dir, "i18n", "jdAppdataEdit_" + system_language + ".qm"))
+        app_translator.load(os.path.join(app_trans_dir, "jdAppdataEdit_" + system_language.split("_")[0] + ".qm"))
+        app_translator.load(os.path.join(app_trans_dir, "jdAppdataEdit_" + system_language + ".qm"))
+        qt_translator.load(os.path.join(qt_trans_dir, "qt_" + system_language.split("_")[0] + ".qm"))
+        qt_translator.load(os.path.join(qt_trans_dir, "qt_" + system_language + ".qm"))
     else:
-        translator.load(os.path.join(env.program_dir, "i18n", "jdAppdataEdit_" + language + ".qm"))
-    app.installTranslator(translator)
+        app_translator.load(os.path.join(app_trans_dir, "jdAppdataEdit_" + language + ".qm"))
+        qt_translator.load(os.path.join(qt_trans_dir, "qt_" + language.split("_")[0] + ".qm"))
+        qt_translator.load(os.path.join(qt_trans_dir, "qt_" + language + ".qm"))
+    app.installTranslator(app_translator)
+    app.installTranslator(qt_translator)
 
     env.translate_window = TranslateWindow(env)
 
