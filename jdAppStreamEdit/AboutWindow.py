c745bb0bec6067eddce742747b960c2333205848
JakobDev <jakobdev@gmx.de>
2023-03-09 16:49:16 +0100

    Update AboutWindow

--- jdAppStreamEdit/AboutWindow.py
+++ jdAppStreamEdit/AboutWindow.py
@@ -10,7 +10,7 @@ class AboutWindow(QDialog):
         uic.loadUi(os.path.join(env.program_dir, "AboutWindow.ui"), self)
 
         self.icon_label.setPixmap(env.icon.pixmap(64, 64))
-        self.version_label.setText(self.version_label.text().replace("{version}", env.version))
+        self.version_label.setText(self.version_label.text().replace("{{version}}", env.version))
 
-        self.view_source_button.clicked.connect(lambda: webbrowser.open("https://gitlab.com/JakobDev/jdAppdataEdit"))
+        self.view_source_button.clicked.connect(lambda: webbrowser.open("https://codeberg.org/JakobDev/jdAppStreamEdi"))
         self.close_button.clicked.connect(self.close)
