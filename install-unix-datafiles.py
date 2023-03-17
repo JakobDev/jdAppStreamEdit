653a245186167f9901a7bc33346588b8cb176c55
JakobDev <jakobdev@gmx.de>
2023-03-17 10:10:37 +0100

    Try to integrate Unix datafiles into Weblate

--- install-unix-datafiles.py
+++ install-unix-datafiles.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import subprocess
 import argparse
 import shutil
 import sys
@@ -22,7 +23,20 @@ def install_data_file(src: str, dest: str) -> None:
         sys.exit(1)
 
 
+def create_directory(path: str) -> None:
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
+    except PermissionError as ex:
+        print(f"Don't have Permission to create {ex.filename}. Use --prefix to change the Prefix.", file=sys.stderr)
+
+
 def main() -> None:
+    if not shutil.which("msgfmt"):
+        print("msgfmt not found. You need to install gettext for the installation.")
+        sys.exit(1)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--prefix", default="/usr", help="The Prefix to install the files")
     args = parser.parse_args()
@@ -30,11 +44,15 @@ def main() -> None:
     project_root = os.path.dirname(__file__)
 
     install_data_file(os.path.join(project_root, "jdAppStreamEdit", "Icon.svg"), os.path.join(args.prefix, "share", "icons", "hicolor", "scalable", "apps", "page.codeberg.JakobDev.jdAppStreamEdit.svg"))
-    install_data_file(os.path.join(project_root, "deploy", "page.codeberg.JakobDev.jdAppStreamEdit.desktop"), os.path.join(args.prefix, "share", "applications", "page.codeberg.JakobDev.jdAppStreamEdit.desktop"))
-    install_data_file(os.path.join(project_root, "deploy", "page.codeberg.JakobDev.jdAppStreamEdit.ExternalReleasesEditor.desktop"), os.path.join(args.prefix, "share", "applications", "page.codeberg.JakobDev.jdAppStreamEdit.ExternalReleasesEditor.desktop"))
     install_data_file(os.path.join(project_root, "deploy", "x-freedesktop-appstream.xml"), os.path.join(args.prefix, "share", "mime", "packages", "page.codeberg.JakobDev.jdAppStreamEdit.FreedesktopAppStream.xml"))
     install_data_file(os.path.join(project_root, "deploy", "x-freedesktop-appstream-releases.xml"), os.path.join(args.prefix, "share", "mime", "packages", "page.codeberg.JakobDev.jdAppStreamEdit.FreedesktopAppStreamReleases.xml"))
-    install_data_file(os.path.join(project_root, "deploy", "page.codeberg.JakobDev.jdAppStreamEdit.metainfo.xml"), os.path.join(args.prefix, "share", "metainfo", "page.codeberg.JakobDev.jdAppStreamEdit.metainfo.xml"))
+
+    create_directory(os.path.join(args.prefix, "share", "applications"))
+    create_directory(os.path.join(args.prefix, "share", "metainfo"))
+
+    subprocess.run(["msgfmt", "--desktop", "--template", os.path.join(project_root, "deploy", "page.codeberg.JakobDev.jdAppStreamEdit.desktop"), "-d", os.path.join(project_root, "deploy", "translations"), "-o",  os.path.join(args.prefix, "share", "applications", "page.codeberg.JakobDev.jdAppStreamEdit.desktop")], check=True)
+    subprocess.run(["msgfmt", "--desktop", "--template", os.path.join(project_root, "deploy", "page.codeberg.JakobDev.jdAppStreamEdit.ExternalReleasesEditor.desktop"), "-d", os.path.join(project_root, "deploy", "translations"), "-o",  os.path.join(args.prefix, "share", "applications", "page.codeberg.JakobDev.jdAppStreamEdit.ExternalReleasesEditor.desktop")], check=True)
+    subprocess.run(["msgfmt", "--xml", "--template", os.path.join(project_root, "deploy", "page.codeberg.JakobDev.jdAppStreamEdit.metainfo.xml"), "-d", os.path.join(project_root, "deploy", "translations"), "-o",  os.path.join(args.prefix, "share", "metainfo", "page.codeberg.JakobDev.jdAppStreamEdit.metainfo.xml")], check=True)
 
 
 if __name__ == "__main__":
---
+++ tools/UpdateUnixDataTranslations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import subprocess
+import pathlib
+
+
+def main() -> None:
+    deploy_path = pathlib.Path(__file__).parent .parent/ "deploy"
+    pot_path = str(deploy_path / "translations" / "messages.pot")
+
+    subprocess.run(["xgettext", "-l", "xml", "--its", str(deploy_path / "translations" / "AppStream.its"), "--no-location", "-o", pot_path, str(deploy_path / "page.codeberg.JakobDev.jdAppStreamEdit.metainfo.xml")], check=True)
+    subprocess.run(["xgettext", "-l", "desktop", "-k", "-kComment", "--no-location", "-o", pot_path, "-j", str(deploy_path / "page.codeberg.JakobDev.jdAppStreamEdit.desktop")], check=True)
+    subprocess.run(["xgettext", "-l", "desktop", "-k", "-kComment", "--no-location", "-o", pot_path, "-j", str(deploy_path / "page.codeberg.JakobDev.jdAppStreamEdit.ExternalReleasesEditor.desktop")], check=True)
+
+    for file in (deploy_path / "translations").iterdir():
+        if file.suffix == ".po":
+            subprocess.run(["msgmerge", "-o", str(file), str(file), pot_path], check=True)
+
+
+if __name__ == "__main__":
+    main()
