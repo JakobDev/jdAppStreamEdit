79fa99472026bb23fa753af6ddd3c5e12ba7d1ee
JakobDev <jakobdev@gmx.de>
2023-05-31 12:16:57 +0200

    Add Windows Installer

--- jdAppStreamEdit.py
+++ jdAppStreamEdit.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
-from jdAppStreamEdit import jdAppStreamEdit
-jdAppStreamEdit()
+from jdAppStreamEdit import jdAppStreamEdit, ExternalReleases
+import sys
+import os
+
+
+if getattr(sys, "frozen", False) and os.path.basename(sys.executable) == "ExternalReleasesEditor.exe":
+    ExternalReleases()
+else:
+    jdAppStreamEdit()
