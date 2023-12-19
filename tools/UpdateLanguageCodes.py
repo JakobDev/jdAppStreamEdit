649c25fbf4a2d8975e8d2d8acbbcb27590593b08
JakobDev <jakobdev@gmx.de>
2023-12-19 14:25:31 +0100

    Some style fixes

--- tools/UpdateLanguageCodes.py
+++ tools/UpdateLanguageCodes.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
+import pathlib
+import iso639
 import csv
-from pathlib import Path
-
-from iso639 import iter_langs
 
 
-def main():
-    root_path = Path(__file__).parent.parent
+def main() -> None:
+    root_path = pathlib.Path(__file__).parent.parent
     csvfile = root_path / "jdAppStreamEdit" / "data" / "language_codes.csv"
 
     with open(csvfile, "w", encoding="utf8", newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["name", "alpha2"])
-        for lang in iter_langs():
+        for lang in iso639.iter_langs():
             if lang.pt1:
                 writer.writerow([lang.name, lang.pt1])
 
